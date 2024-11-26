963ba17d269bbb3ec10fa92919ec904ab3ec9c2e
Param <param23403@gmail.com>
2024-11-25 21:54:18 -0500

    routing change

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,31 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import "./index.css"
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
 
-import Login from "./pages/Login"
-import Landing from "./pages/Landing"
+import Login from "./pages/Login";
+import Landing from "./pages/Landing";
 
 const router = createBrowserRouter([
-	{
-		element: <Layout/>,
-		children: [
-			{
-				path: "/test",
-				element: <Landing/>
-			},
-			{
-				path: "login",
-				element: <Login/>
-			},
-		]
-	},
-])
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
-)
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
