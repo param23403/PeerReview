c0fdc599aeca408e2ae4434324ee8ca487586bfe
danbiondolillo23 <danbiondolillo23@gmail.com>
2024-12-17 17:58:33 -0500

    update local api route in constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:8888/.netlify/functions/api'
+  ? 'http://localhost:8888/api'
   : '/.netlify/functions/api';
