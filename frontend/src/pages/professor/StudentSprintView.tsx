1920f42e7fcce22ea4e1a5035fb2c07bca4aa59b
Param <param23403@gmail.com>
2025-02-05 02:42:05 -0500

    backs added

--- frontend/src/pages/professor/StudentSprintView.tsx
+++ frontend/src/pages/professor/StudentSprintView.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Button } from "../../components/ui/button";
 export default function StudentSprintView() {
   const { computingId } = useParams<{ computingId: string }>();
   const { sprintId } = useParams<{ sprintId: string }>();
+  const navigate = useNavigate();
 
+  const handleBackClick = () => {
+    if (window.history.length > 2) {
+      navigate(-1);
+    } else {
+      navigate("/", { replace: true });
+    }
+  };
   return (
     <div className="container mx-auto p-4 flex justify-center">
+      <Button onClick={handleBackClick}>Back</Button>
       <div className="w-full max-w-3xl">
         <div className="space-y-8">Hi</div>
       </div>
